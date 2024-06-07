cc6e0c208d60acd431ccb0a76bcb0d33caa5a733
Sunggggggg <sky990319@gmail.com>
2024-06-07 17:57:24 +0900

    f

--- lib/models/Motion/README.md
+++
@@ -1,1 +0,0 @@
-Discrete Cosine Transformation을 통해 High-frequency를 제거한 Signal을 통해 Reconstruction
---
+++ lib/models/Motion_mb/model.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def forward(self, ):
+
+        return
