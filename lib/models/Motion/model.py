c6b5c6a19db7c1a09080d0f296562a7cbcdb275f
Sunggggggg <sky990319@gmail.com>
2024-06-07 16:44:28 +0900

    f

--- lib/models/Motion/model.py
+++ lib/models/Motion/model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 from lib.models.Motion.jointspace import JointTree
 from lib.models.Motion.encoder import Encoder
-from lib.models.Motion.decoder import Decoder
+#from lib.models.Motion.decoder import Decoder
 
 class Model(nn.Module) :
     def __init__(self, 
@@ -20,7 +20,7 @@ class Model(nn.Module) :
         self.joint_space = JointTree()
 
         self.encoder = Encoder(seqlen, num_joint=num_joint, embed_dim=embed_dim, t_encoder_depth=t_encoder_depth, j_encoder_depth=j_encoder_depth)
-        self.decoder = Decoder(seqlen=16, num_joint=19, embed_dim=64)
+        #self.decoder = Decoder(seqlen=16, num_joint=19, embed_dim=64)
 
     def forward(self, img_feat, vitpose_j2d, img_path=None):
         """
