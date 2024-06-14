6a8c4c9849b94618712ab80e8badac333580a92f
Sunggggggg <sky990319@gmail.com>
2024-06-14 19:39:26 +0900

    f

--- test.py
+++ test.py
@@ -1,8 +1,37 @@
+import os
+os.environ['PYOPENGL_PLATFORM'] = 'egl'
+
 import torch
-from lib.models.Motion_mb.model import Model
+import pprint
+import random
+import numpy as np
+import torch.backends.cudnn as cudnn
+from lib.dataset._loaders import get_data_loaders
+from lib.utils.utils import create_logger
 
-model = Model(16, 17)
-x = torch.rand((1, 16, 2048))
-y = torch.rand((1, 16, 17, 2))
+def main(cfg):
+    if cfg.SEED_VALUE >= 0:
+        print(f'Seed value for the experiment {cfg.SEED_VALUE}')
+        os.environ['PYTHONHASHSEED'] = str(cfg.SEED_VALUE)
+        random.seed(cfg.SEED_VALUE)
+        torch.manual_seed(cfg.SEED_VALUE)
+        np.random.seed(cfg.SEED_VALUE)
+        torch.cuda.manual_seed(cfg.SEED_VALUE)
+        torch.cuda.manual_seed_all(cfg.SEED_VALUE)
+        
+    logger = create_logger(cfg.LOGDIR, phase='train')
 
-model(x, y)
+    logger.info(f'GPU name -> {torch.cuda.get_device_name()}')
+    logger.info(f'GPU feat -> {torch.cuda.get_device_properties("cuda")}')
+
+    logger.info(pprint.pformat(cfg))
+
+    # cudnn related setting
+    cudnn.benchmark = cfg.CUDNN.BENCHMARK
+    torch.backends.cudnn.deterministic = cfg.CUDNN.DETERMINISTIC
+    torch.backends.cudnn.enabled = cfg.CUDNN.ENABLED
+
+    # ========= Dataloaders ========= #
+    data_loaders = get_data_loaders(cfg)
+
+    
