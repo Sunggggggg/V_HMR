84180295c7f874251964fbeac1114e174b9dd9e6
Sunggggggg <sky990319@gmail.com>
2024-06-26 16:17:12 +0900

    f

--- lib/models/FCM/model.py
+++ lib/models/FCM/model.py
@@ -2,18 +2,20 @@ import torch
 import torch.nn as nn
 from functools import partial 
 from .jointspace import JointTree
-from .transformer import TemporalEncoder, JointEncoder, FreqTempEncoder, CrossAttention, Transformer, STEncoder, FreqTempEncoder_img
-from .regressor import LocalRegressorThetaBeta, GlobalRegressor, NewLocalRegressor
+from .transformer import FreqTempEncoder, CrossAttention, Transformer
+from .regressor import GlobalRegressor, NewLocalRegressor
+from .FeatureCorrection import ImageFeatureCorrection
 
 """
-Feature Correction Model
+PoseformerV2 사용
+GMM 사용 X
 """
 
 class Model(nn.Module):
     def __init__(self, 
                  num_coeff_kept=8,
                  num_frames=16,
-                 num_joints=24,
+                 num_joints=19,
                  embed_dim=512,
                  num_heads=8,
                  depth=4,
@@ -21,6 +23,7 @@ class Model(nn.Module):
 
                  ):
         super().__init__()
+        self.stride = 4
         self.mid_frame = num_frames//2
         self.jointtree = JointTree()
         # Temp transformer
@@ -28,7 +31,8 @@ class Model(nn.Module):
         self.temp_encoder = Transformer(depth=3, embed_dim=embed_dim)
         
         # Spatio transformer
-        self.joint_encoder = JointEncoder(num_joint=num_joints)
+        #self.joint_encoder = JointEncoder(num_joint=num_joints)
+        self.joint_encoder = FreqTempEncoder(num_joints, 32, 3, norm_layer=partial(nn.LayerNorm, eps=1e-6), num_coeff_keep=3)
 
         # Global regre
         self.proj_input = nn.Linear(num_joints*32, embed_dim)
@@ -42,8 +46,7 @@ class Model(nn.Module):
         self.joint_refiner = FreqTempEncoder(num_joints, 32, 3, norm_layer=partial(nn.LayerNorm, eps=1e-6), num_coeff_keep=3)
         self.proj_short_joint = nn.Linear(num_joints*32, embed_dim//2)
         self.proj_short_img = nn.Linear(2048, embed_dim//2)
-        self.temp_local_encoder = Transformer(depth=2, embed_dim=embed_dim//2, length=3)
-        #self.temp_local_encoder = FreqTempEncoder_img(embed_dim//2, 3, norm_layer=partial(nn.LayerNorm, eps=1e-6), num_coeff_keep=3)
+        self.temp_local_encoder = ImageFeatureCorrection(embed_dim//2)
 
         self.local_decoder = CrossAttention(embed_dim//2)
         self.local_regressor = NewLocalRegressor(embed_dim//2)
@@ -60,9 +63,8 @@ class Model(nn.Module):
         f_temp = self.temp_encoder(f_temp)                          # [B, T, 512]
 
         # Joint transformer
-        vitpose_2d = self.jointtree.add_joint(vitpose_2d[..., :2])  # [B, T, 19, 2] 
-        vitpose_2d = self.jointtree.mapping(vitpose_2d)             # [B, T, 24, 2] 
-        f_joint = self.joint_encoder(vitpose_2d)                    # [B, T, 768(24*32)]
+        vitpose_2d = self.jointtree.add_joint(vitpose_2d[..., :2])      # [B, T, 19, 2] 
+        f_joint = self.joint_encoder(vitpose_2d, vitpose_2d, 16)        # [B, T, 768(24*32)]
         f_joint = self.proj_input(f_joint)
         
         f = self.norm_input(f_joint + f_temp)   # [B, T, 512]
@@ -77,15 +79,14 @@ class Model(nn.Module):
 
         # 
         full_2d_joint, short_2d_joint = vitpose_2d, vitpose_2d[:, self.mid_frame-1:self.mid_frame+2]
-        short_f_joint = self.joint_refiner(full_2d_joint, short_2d_joint).flatten(-2)       # [B, 3, 768]
-        short_f_joint = self.proj_short_joint(short_f_joint)
+        short_f_joint = self.joint_refiner(full_2d_joint, short_2d_joint)               # [B, 3, 768]
+        short_f_joint = self.proj_short_joint(short_f_joint)                            # [B, 3, 256]
         
-        f_img_short = f_img[:, self.mid_frame-1:self.mid_frame+2]
-        f_img_short = self.proj_short_img(f_img_short)
-        #f_img_short = self.temp_local_encoder(f_img, f_img_short)
-        f_img_short = self.temp_local_encoder(f_img_short)          # [B, 3, 256]
+        short_f_img = f_img[:, self.mid_frame-self.stride:self.mid_frame+self.stride+1] # [B, 8, 2048]
+        short_f_img = self.proj_short_img(short_f_img)
+        short_f_img = self.temp_local_encoder(short_f_img, short_f_img[:, self.stride-1:self.stride+2]) # [B, 6, 256]
 
-        f_st = self.local_decoder(short_f_joint, f_img_short)
+        f_st = self.local_decoder(short_f_joint, short_f_img)
 
         if is_train :
             f_st = f_st
--- lib/models/FCM/transformer.py
+++ lib/models/FCM/transformer.py
@@ -42,123 +42,6 @@ class Transformer(nn.Module):
 
         return x
 
-class TemporalEncoder(nn.Module):
-    def __init__(self, depth=3, embed_dim=512, mlp_hidden_dim=1024,
-            h=8, drop_rate=0.1, drop_path_rate=0.2, attn_drop_rate=0., length=16):
-        super().__init__()
-        qkv_bias = True
-        qk_scale = None
-        # load mean smpl pose, shape and cam
-        mean_params = np.load(SMPL_MEAN_PARAMS)
-        self.init_pose = torch.from_numpy(mean_params['pose'][:]).unsqueeze(0).to('cuda')
-        self.init_shape = torch.from_numpy(mean_params['shape'][:].astype('float32')).unsqueeze(0).to('cuda')
-        self.init_cam = torch.from_numpy(mean_params['cam']).unsqueeze(0).to('cuda')
-        
-        self.mask_token_mlp = nn.Sequential(
-            nn.Linear(24 * 6 + 13, 256),
-            nn.ReLU(),
-            nn.Linear(256, 512),
-            nn.ReLU(),
-            nn.Linear(512, embed_dim // 2)
-        )
-        norm_layer = partial(nn.LayerNorm, eps=1e-6)
-        self.pos_embed = nn.Parameter(torch.zeros(1, length, embed_dim))
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  
-        
-        self.blocks = nn.ModuleList([
-            Block(
-                dim=embed_dim, num_heads=h, mlp_hidden_dim=mlp_hidden_dim, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
-            for i in range(depth)]) 
-        self.norm = norm_layer(embed_dim)
-
-        self.decoder_blocks = nn.ModuleList([
-            Block(
-                dim=embed_dim // 2, num_heads=h, mlp_hidden_dim=embed_dim * 2, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
-            for i in range(depth // 2)])
-        self.decoder_embed = nn.Linear(embed_dim, embed_dim // 2, bias=True)
-        self.decoder_pos_embed = nn.Parameter(torch.zeros(1, length, embed_dim // 2))
-        self.decoder_norm = norm_layer(embed_dim // 2)
-
-    def forward(self, x, is_train=True, mask_ratio=0.):
-        if is_train:
-            latent, mask, ids_restore = self.forward_encoder(x, mask_flag=True, mask_ratio=mask_ratio)
-            pred = self.forward_decoder(latent, ids_restore)  # [N, L, p*p*3]
-        else:
-            latent, mask, ids_restore = self.forward_encoder(x, mask_flag=False,mask_ratio=0.)
-            pred = self.forward_decoder(latent, ids_restore)  # [N, L, p*p*3]
-        return pred, mask, latent
-
-    def forward_encoder(self, x, mask_flag=False, mask_ratio=0.):
-        x = x + self.pos_embed
-        if mask_flag:
-            x, mask, ids_restore = self.random_masking(x, mask_ratio)
-            # print('mask')
-        else:
-            mask = None
-            ids_restore = None
-
-        for blk in self.blocks:
-            x = blk(x)
-        x = self.norm(x)
-        return x, mask, ids_restore
-
-    def forward_decoder(self, x, ids_restore):
-        # embed tokens
-        x = self.decoder_embed(x)
-        if ids_restore is not None:
-            mean_pose = torch.cat((self.init_pose, self.init_shape, self.init_cam), dim=-1)
-            # append mask tokens to sequence
-            mask_tokens = self.mask_token_mlp(mean_pose)
-            mask_tokens = mask_tokens.repeat(x.shape[0], ids_restore.shape[1] - x.shape[1], 1)
-            x_ = torch.cat([x, mask_tokens], dim=1)  # no cls token
-            x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
-        else:
-            x_ = x
-        x = x_ + self.decoder_pos_embed
-
-        # apply Transformer blocks
-        for blk in self.decoder_blocks:
-            x = blk(x)
-        x = self.decoder_norm(x)
-        return x
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            # we use xavier_uniform following official JAX ViT:
-            torch.nn.init.xavier_uniform_(m.weight)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-
-    def random_masking(self, x, mask_ratio):
-        """
-        Perform per-sample random masking by per-sample shuffling.
-        Per-sample shuffling is done by argsort random noise.
-        x: [N, L, D], sequence
-        """
-        N, L, D = x.shape  # batch, length, dim
-        len_keep = int(L * (1 - mask_ratio))
-        
-        noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
-        
-        # sort noise for each sample
-        ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
-        ids_restore = torch.argsort(ids_shuffle, dim=1)
-        # keep the first subset
-        ids_keep = ids_shuffle[:, :len_keep]
-        x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
-        # generate the binary mask: 0 is keep, 1 is remove
-        mask = torch.ones([N, L], device=x.device, dtype=torch.bool)
-        mask[:, :len_keep] = 0
-        # unshuffle to get the binary mask
-        mask = torch.gather(mask, dim=1, index=ids_restore).unsqueeze(-1) # assgin value from ids_restore
-        
-        return x_masked, mask, ids_restore
-
 class JointEncoder(nn.Module) :
     def __init__(self, num_joint=24, emb_dim=32, depth=3, num_heads=4, drop_rate=0.) :
         super().__init__()
@@ -269,38 +152,6 @@ class CrossAttention(nn.Module):
         x = self.proj_drop(x)
         return x
 
-class FreqTempBlock(nn.Module):
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
-        super().__init__()
-        # Cross Attention
-        self.norm_t = norm_layer(dim)
-        self.norm_f = norm_layer(dim)
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        
-        # Temp1
-        self.norm1 = norm_layer(dim)
-        self.norm2 = norm_layer(dim)
-
-        self.cross_attn = CrossAttention(dim, num_heads=8, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
-        self.self_attn = Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
-
-    def forward(self, f_temp, f_freq):
-        """
-        f_temp : [B, 3, dim]
-        f_freq : [B, t, dim]
-        """
-        f_temp = self.norm_t(f_temp)
-        f_freq = self.norm_f(f_freq)
-
-        f_temp = f_temp + self.drop_path(self.cross_attn(f_temp, f_freq))
-        f_temp = f_temp + self.drop_path(self.self_attn(self.norm1(f_temp)))
-        f_temp = f_temp + self.drop_path(self.mlp(self.norm2(f_temp)))  
-
-        return f_temp
-
 class FreqMlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
         super().__init__()
@@ -387,7 +238,7 @@ class FreqTempEncoder(nn.Module) :
         freq_feat = self.freq_embedding(freq_feat)
         freq_feat = freq_feat + self.freq_pos_embedding     # [B, k, J*32]
 
-        init_joint_feat = joint_feat = self.joint_embedding(joint_feat).reshape(B*T, J, -1)   # [B3, J, 32]
+        joint_feat = self.joint_embedding(joint_feat).reshape(B*T, J, -1)   # [B3, J, 32]
         joint_feat = joint_feat + self.joint_pos_embedding                  # [B3, J, 32]
         joint_feat = joint_feat.reshape(B, T, J, -1).view(B, T, -1)         # [B, T, J*32]
         f = torch.cat([joint_feat, freq_feat], dim=1)                       # [B, T+k, J*32]
@@ -397,8 +248,6 @@ class FreqTempEncoder(nn.Module) :
         
         joint_feat, freq_feat = f[:, :num_imgs], f[:, num_imgs:]   # [B, 3, J*32], [B, k, J*32]
         joint_feat = joint_feat + self.head(joint_feat, freq_feat)
-        #joint_feat = init_joint_feat.reshape(B, T, -1) + self.head(joint_feat, freq_feat)
-        joint_feat = joint_feat.reshape(B, T, J, -1)
         return joint_feat
 
 class FreqTempEncoder_img(nn.Module) :
@@ -454,82 +303,3 @@ class FreqTempEncoder_img(nn.Module) :
         img_feat, freq_feat = f[:, :3], f[:, 3:]                # [B, 3, 256], [B, k, 256]
         img_feat = img_feat + self.head(img_feat, freq_feat)    # []
         return img_feat
-
-class STEncoder(nn.Module):
-    def __init__(self, 
-                 num_frames=16,
-                 num_joints=20,
-                 embed_dim=512, 
-                 depth=3, 
-                 num_heads=8, 
-                 mlp_ratio=2, 
-                 drop_rate=0., 
-                 attn_drop_rate=0., 
-                 drop_path_rate=0.2
-                 ) :
-        super().__init__()
-        self.depth = depth
-
-        self.joint_embed = nn.Linear(32, embed_dim)
-        self.img_embed = nn.Linear(2048, embed_dim)
-
-        self.temp_embed = nn.Linear(embed_dim, embed_dim)
-        self.s_norm = nn.LayerNorm(embed_dim)
-        self.t_norm = nn.LayerNorm(embed_dim)
-
-        self.spatial_pos_embed = nn.Parameter(torch.zeros(1, num_joints, embed_dim))
-        self.temporal_pos_embed = nn.Parameter(torch.zeros(1, num_frames, embed_dim))
-        self.pos_drop = nn.Dropout(p=drop_rate)
-
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  
-        self.s_blocks = nn.ModuleList([
-            Block(
-                dim=embed_dim, num_heads=num_heads, mlp_hidden_dim=embed_dim*mlp_ratio,
-                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i])
-            for i in range(depth)])
-        
-        self.t_blocks = nn.ModuleList([
-            Block(
-                dim=embed_dim, num_heads=num_heads, mlp_hidden_dim=embed_dim*mlp_ratio,
-                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i])
-            for i in range(depth)])
-
-    def forward(self, f_temp, f_joint):
-        """
-        f_temp  : [B, T, D]
-        f_joint : [B, T, J, 32]
-        """
-        B, T, J, _ = f_joint.shape
-        f_joint = self.joint_embed(f_joint)         # [B, T, J, D]
-        f_temp = self.img_embed(f_temp)[:, :, None] # [B, T, 1, D]
-        f = f_joint + f_temp                        # [B, T, J, D]
-
-        # Spatial
-        f = rearrange(f, 'b t j c  -> (b t) j c')   # [BT, J, D]
-        f = f + self.spatial_pos_embed
-        f = self.pos_drop(f)
-        f = self.s_blocks[0](f)                     # [BT, J, D]
-        f = self.s_norm(f)
-
-        # Temporal
-        f = rearrange(f, '(b t) j c -> (b j) t c', t=T) #[BJ, T, D]
-        f = f + self.temporal_pos_embed             # [BJ, T, D]
-        f = self.pos_drop(f)
-        f = self.t_blocks[0](f)                     # [BJ, T, D]
-        f = self.t_norm(f)
-
-        # Loop
-        for i in range(1, self.depth):
-            s_block = self.s_blocks[i]
-            t_block = self.t_blocks[i]
-
-            f = rearrange(f, '(b j) t c -> (b t) j c', j=J)
-            f = s_block(f)
-            f = self.s_norm(f)
-
-            f = rearrange(f, '(b t) j c -> (b j) t c', t=T)
-            f = t_block(f)
-            f = self.t_norm(f)
-
-        f = rearrange(f, '(b j) t c -> b t j c', j=J)
-        return f
