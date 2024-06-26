4c609c797c3e833107cc65947c43fc067da3503c
Sunggggggg <sky990319@gmail.com>
2024-06-26 10:46:54 +0900

    f

--- lib/core/Disen/trainer.py
+++ lib/core/Disen/trainer.py
@@ -231,9 +231,8 @@ class Trainer():
                 # <=============
                 input_feat = target['features'].cuda()
                 input_pose = target['vitpose_j2d'].cuda()
-                input_text = target['text_emb'].cuda()
 
-                smpl_output = self.generator(input_text, input_feat, input_pose, is_train=False, J_regressor=J_regressor)
+                smpl_output = self.generator(input_feat, input_pose, is_train=False, J_regressor=J_regressor)
             
                 # convert to 14 keypoint format for evaluation
                 n_kp = smpl_output[-1]['kp_3d'].shape[-2]
