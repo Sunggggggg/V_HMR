0e22477f0cc5b9e91ea881e3cbdb98dc983c8827
sky <sky990319@gmail.com>
2024-06-28 00:52:21 +0900

    f

--- lib/models/LF/model.py
+++ lib/models/LF/model.py
@@ -1,10 +1,13 @@
+import os
 import torch
 import torch.nn as nn
 from functools import partial 
 from .jointspace import JointTree
 from .transformer import FreqTempEncoder
 from .FeatureCorrection import ImageFeatureCorrection
-from .regressor import HSCR
+from .regressor import HSCR, Regressor
+
+from lib.core.config import BASE_DATA_DIR
 
 class Model(nn.Module):
     def __init__(self, 
@@ -15,7 +18,7 @@ class Model(nn.Module):
                  num_heads=8,
                  depth=4,
                  drop_rate=0.,
-
+                 pretrained=os.path.join(BASE_DATA_DIR, 'spin_model_checkpoint.pth.tar'),
                  ):
         super().__init__()
         self.mid_frame = num_frames//2
@@ -26,10 +29,17 @@ class Model(nn.Module):
 
         # Temporal encoder
         self.temp_encoder = ImageFeatureCorrection(embed_dim)
+        self.output_proj = nn.Linear(embed_dim, 2048)
 
         # Regressor
         self.pos_emb = nn.Linear(3, 128)
-        self.regressor = HSCR(f_dim=embed_dim)
+        self.regressor1 = Regressor()
+        self.regressor2 = HSCR(f_dim=embed_dim)
+        if pretrained and os.path.isfile(pretrained):
+            pretrained_dict = torch.load(pretrained)['model']
+
+            self.regressor.load_state_dict(pretrained_dict, strict=False)
+            print(f'=> loaded pretrained model from \'{pretrained}\'')
         
         self.apply(self._init_weights)
         
@@ -53,13 +63,15 @@ class Model(nn.Module):
         # 3D Lifting
         pose2d = self.joint_tree.add_joint(pose2d[..., :2])
         pose3d = self.lifter(pose2d, pose2d[:, self.mid_frame-1:self.mid_frame+2])          # [B, 1, 19, 3]
+        pose3d = self.pos_emb(pose3d)
         # 
         f_temp = self.temp_encoder(f_img[:, self.mid_frame-self.stride:self.mid_frame+self.stride+1]
                                    , f_img[:, self.mid_frame-1:self.mid_frame+2])           # [B, 1, dim]
 
         # [B, 1, *]
-        pose3d = self.pos_emb(pose3d)
-        smpl_output = self.regressor(f_temp, pose3d, is_train=is_train, J_regressor=J_regressor)
+        f_out = self.output_proj(f_temp)
+        _, (pred_pose, pred_shape, pred_cam) = self.regressor1(f_out, pose3d, is_train=is_train, J_regressor=J_regressor)
+        smpl_output = self.regressor2(f_temp, pose3d, pred_pose, pred_shape, pred_cam, is_train=is_train, J_regressor=J_regressor)
     
         scores = None
         if not is_train:
--- lib/models/LF/regressor.py
+++ lib/models/LF/regressor.py
@@ -112,28 +112,22 @@ class HSCR(nn.Module):
 
         self.local_reg = KTD(embed_dim, hidden_dim)
 
-    def forward(self, x, embed_3djoint, is_train=False, J_regressor=None):
-        batch_size, T = x.shape[:2]
-        init_pose = self.init_pose.expand(batch_size, T, -1)
-        init_shape = self.init_shape.expand(batch_size, T, -1)
-        init_cam = self.init_cam.expand(batch_size, T, -1)
-        # print("init",init_pose.shape)
+    def forward(self, x, embed_3djoint, init_pose, init_shape, init_cam, is_train=False, J_regressor=None):
         pred_pose = init_pose.detach()
         pred_shape = init_shape.detach()
         pred_cam = init_cam.detach()
+        
+        xc_shape_cam = torch.cat([x, pred_shape], -1)
+        xc_pose_cam = torch.cat([x, pred_pose], -1)
+        xc_shape_cam = self.fc1(xc_shape_cam)
+        xc_shape_cam = self.drop1(xc_shape_cam)
 
-        for i in range(3):
-            xc_shape_cam = torch.cat([x, pred_shape], -1)
-            xc_pose_cam = torch.cat([x, pred_pose], -1)
-            xc_shape_cam = self.fc1(xc_shape_cam)
-            xc_shape_cam = self.drop1(xc_shape_cam)
+        xc_pose_cam = self.fc2(xc_pose_cam)
+        xc_pose_cam = self.drop2(xc_pose_cam)
 
-            xc_pose_cam = self.fc2(xc_pose_cam)
-            xc_pose_cam = self.drop2(xc_pose_cam)
-
-            pred_pose = self.local_reg(xc_pose_cam, embed_3djoint, pred_pose) + pred_pose
-            pred_shape = self.decshape(torch.cat([xc_shape_cam, pred_shape], dim=-1)) + pred_shape
-            pred_cam = self.deccam(torch.cat([xc_pose_cam, xc_shape_cam, pred_cam], -1)) + pred_cam
+        pred_pose = self.local_reg(xc_pose_cam, embed_3djoint, pred_pose) + pred_pose
+        pred_shape = self.decshape(torch.cat([xc_shape_cam, pred_shape], dim=-1)) + pred_shape
+        pred_cam = self.deccam(torch.cat([xc_pose_cam, xc_shape_cam, pred_cam], -1)) + pred_cam
 
         pred_pose = pred_pose.reshape(-1, 144)
         pred_shape = pred_shape.reshape(-1, 10)
@@ -173,3 +167,101 @@ class HSCR(nn.Module):
             'rotmat': pred_rotmat
         }]
         return output
+    
+class Regressor(nn.Module):
+    def __init__(self, smpl_mean_params=SMPL_MEAN_PARAMS):
+        super(Regressor, self).__init__()
+
+        npose = 24 * 6  # 144
+
+        self.fc1 = nn.Linear(512 * 4 + npose + 13, 1024)
+        self.drop1 = nn.Dropout()
+        self.fc2 = nn.Linear(1024, 1024)
+        self.drop2 = nn.Dropout()
+
+        self.decpose = nn.Linear(1024, npose)
+        self.decshape = nn.Linear(1024, 10)
+        self.deccam = nn.Linear(1024, 3)
+        nn.init.xavier_uniform_(self.decpose.weight, gain=0.01)
+        nn.init.xavier_uniform_(self.decshape.weight, gain=0.01)
+        nn.init.xavier_uniform_(self.deccam.weight, gain=0.01)
+
+        self.smpl = SMPL(
+            SMPL_MODEL_DIR,
+            batch_size=64,
+            create_transl=False,
+        )
+
+        mean_params = np.load(smpl_mean_params)
+        init_pose = torch.from_numpy(mean_params['pose'][:]).unsqueeze(0)
+        init_shape = torch.from_numpy(mean_params['shape'][:].astype('float32')).unsqueeze(0)
+        init_cam = torch.from_numpy(mean_params['cam']).unsqueeze(0)
+        self.register_buffer('init_pose', init_pose)
+        self.register_buffer('init_shape', init_shape)
+        self.register_buffer('init_cam', init_cam)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def forward(self, x, init_pose=None, init_shape=None, init_cam=None, n_iter=3, is_train=False, J_regressor=None):
+        seq_len = x.shape[1]
+        x = x.reshape(-1, x.size(-1))
+        batch_size = x.shape[0]
+        if init_pose is None:
+            init_pose = self.init_pose.expand(batch_size, -1)
+        if init_shape is None:
+            init_shape = self.init_shape.expand(batch_size, -1)
+        if init_cam is None:
+            init_cam = self.init_cam.expand(batch_size, -1)
+
+        pred_pose = init_pose
+        pred_shape = init_shape
+        pred_cam = init_cam
+        for i in range(n_iter):
+            xc = torch.cat([x, pred_pose, pred_shape, pred_cam], 1)
+            xc = self.fc1(xc)
+            xc = self.drop1(xc)
+            # xc = self.fc2(xc)
+            # xc = self.drop2(xc)
+            pred_pose = self.decpose(xc) + pred_pose
+            pred_shape = self.decshape(xc) + pred_shape
+            pred_cam = self.deccam(xc) + pred_cam
+
+        pred_rotmat = rot6d_to_rotmat(pred_pose).view(batch_size, 24, 3, 3)
+
+        pred_output = self.smpl(
+            betas=pred_shape,
+            body_pose=pred_rotmat[:, 1:],
+            global_orient=pred_rotmat[:, 0].unsqueeze(1),
+            pose2rot=False,
+        )
+
+        pred_vertices = pred_output.vertices
+        pred_joints = pred_output.joints
+
+        if not is_train and J_regressor is not None:
+            J_regressor_batch = J_regressor[None, :].expand(pred_vertices.shape[0], -1, -1).to(pred_vertices.device)
+            pred_joints = torch.matmul(J_regressor_batch, pred_vertices)
+            pred_joints = pred_joints[:, H36M_TO_J14, :]
+
+        pred_keypoints_2d = projection(pred_joints, pred_cam)
+
+        pose = rotation_matrix_to_angle_axis(pred_rotmat.reshape(-1, 3, 3)).reshape(-1, 72)
+
+        output = [{
+            'theta'  : torch.cat([pred_cam, pose, pred_shape], dim=1),  # [BT, 3+72+10]
+            'verts'  : pred_vertices,
+            'kp_2d'  : pred_keypoints_2d,
+            'kp_3d'  : pred_joints,
+            'rotmat' : pred_rotmat
+        }]
+
+        
+        return output, (pred_pose, pred_shape, pred_cam)
